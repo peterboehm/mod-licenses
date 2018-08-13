9ef87eedb771282c891c1ed6b47be0cc709f9863
Ian Ibbotson <ianibbo@gmail.com>
2018-08-13 17:06:58 +0100

    Working on service registration

---
+++ service/src/test/groovy/mod/license/LicenseSpec.groovy
@@ -0,0 +1,18 @@
+package mod.license
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class LicenseSpec extends Specification implements DomainUnitTest<License> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
