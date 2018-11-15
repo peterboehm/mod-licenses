749be3ce5bd02b7cf5d098d55a2229b835f158ad
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-11-15 14:54:59 +0000

    Tidy up the constructor calls.

--- service/grails-app/controllers/org/olf/licenses/CustomPropertDefinitionController.groovy
+++ service/grails-app/controllers/org/olf/licenses/CustomPropertDefinitionController.groovy
@@ -1,19 +1,8 @@
 package org.olf.licenses
 
-import com.k_int.web.toolkit.custprops.CustomPropertyDefinition
-import com.k_int.web.toolkit.refdata.GrailsDomainRefdataHelpers
-import com.k_int.web.toolkit.refdata.RefdataCategory
-import com.k_int.web.toolkit.rest.RestfulController
-import com.k_int.web.toolkit.utils.DomainUtils
-
 import grails.gorm.multitenancy.CurrentTenant
-import grails.web.Controller
 import groovy.util.logging.Slf4j
 
 @Slf4j
 @CurrentTenant
-class CustomPropertyDefinitionController extends com.k_int.web.toolkit.CustomPropertyDefinitionController {
-  CustomPropertyDefinitionController() {
-    super(CustomPropertyDefinition)
-  }
-}
+class CustomPropertyDefinitionController extends com.k_int.web.toolkit.CustomPropertyDefinitionController {}
--- service/grails-app/controllers/org/olf/licenses/RefdataController.groovy
+++ service/grails-app/controllers/org/olf/licenses/RefdataController.groovy
@@ -1,20 +1,8 @@
 package org.olf.licenses
 
-import com.k_int.web.toolkit.custprops.CustomPropertyDefinition
-import com.k_int.web.toolkit.refdata.GrailsDomainRefdataHelpers
-import com.k_int.web.toolkit.refdata.RefdataCategory
-import com.k_int.web.toolkit.rest.RestfulController
-import com.k_int.web.toolkit.utils.DomainUtils
-
 import grails.gorm.multitenancy.CurrentTenant
-import grails.web.Controller
 import groovy.util.logging.Slf4j
 
 @Slf4j
 @CurrentTenant
-class RefdataController extends com.k_int.web.toolkit.RefdataController {
-
-  RefdataController() {
-    super(RefdataCategory)
-  }
-}
+class RefdataController extends com.k_int.web.toolkit.RefdataController {}
