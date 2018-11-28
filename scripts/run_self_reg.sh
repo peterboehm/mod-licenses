ff5002f9433e477b4f1278016632b4ad822f94b1
aclseelen <alfons.seelen@hbz-nrw.de>
2018-11-28 15:01:54 +0100

    remove multiline indenting

--- scripts/run_self_reg.sh
+++ scripts/run_self_reg.sh
@@ -3,9 +3,9 @@
 echo Start mod-licenses in self-register mode
 
 java -jar build/libs/mod-licenses-1.0.jar -Xmx1G \
-    --grails.server.host=10.0.2.2 \
-    --selfRegister=on \
-    --dataSource .username=folio_admin \
-    --dataSource .password=folio_admin \
-    --dataSource .url=jdbc:postgresql://localhost:5432/okapi_modules
+--grails.server.host=10.0.2.2 \
+--selfRegister=on \
+--dataSource .username=folio_admin \
+--dataSource .password=folio_admin \
+--dataSource .url=jdbc:postgresql://localhost:5432/okapi_modules
 
