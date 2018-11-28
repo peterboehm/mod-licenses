3a2dbefa10be0c83a4d269998b10f49fec515300
aclseelen <alfons.seelen@hbz-nrw.de>
2018-11-28 15:13:39 +0100

    remove unintended spaces

--- scripts/run_self_reg.sh
+++ scripts/run_self_reg.sh
@@ -5,7 +5,7 @@ echo Start mod-licenses in self-register mode
 java -jar build/libs/mod-licenses-1.0.jar -Xmx1G \
 --grails.server.host=10.0.2.2 \
 --selfRegister=on \
---dataSource .username=folio_admin \
---dataSource .password=folio_admin \
---dataSource .url=jdbc:postgresql://localhost:5432/okapi_modules
+--dataSource.username=folio_admin \
+--dataSource.password=folio_admin \
+--dataSource.url=jdbc:postgresql://localhost:5432/okapi_modules
 
