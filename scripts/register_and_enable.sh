71323ba9fb78b39da0285c33d646eeb2b4680a2e
Ian Ibbotson <ianibbo@googlemail.com>
2018-12-05 10:28:49 +0000

    bump versions on run_external_reg.sh

--- scripts/register_and_enable.sh
+++ scripts/register_and_enable.sh
@@ -11,11 +11,11 @@ if [ ! -d "$DESCRIPTORDIR" ]; then
     ./gradlew generateDescriptors
 fi
 
-curl -XDELETE http://localhost:9130/_/proxy/tenants/diku/modules/mod-licenses-1.0.0
-curl -XDELETE http://localhost:9130/_/discovery/modules/mod-licenses-1.0.0/localhost-mod-licenses-1.0.0
-curl -XDELETE http://localhost:9130/_/proxy/modules/mod-licenses-1.0.0
+curl -XDELETE http://localhost:9130/_/proxy/tenants/diku/modules/mod-licenses-1.1.0
+curl -XDELETE http://localhost:9130/_/discovery/modules/mod-licenses-1.1.0/localhost-mod-licenses-1.0.0
+curl -XDELETE http://localhost:9130/_/proxy/modules/mod-licenses-1.1.0
 # ./gradlew clean generateDescriptors
 curl -XPOST http://localhost:9130/_/proxy/modules -d @"${DESCRIPTORDIR}/ModuleDescriptor.json"
 curl -XPOST http://localhost:9130/_/discovery/modules -d @"${DESCRIPTORDIR}/DeploymentDescriptor.json"
-curl -XPOST http://localhost:9130/_/proxy/tenants/diku/modules -d '{"id": "mod-licenses-1.0.0"}'
+curl -XPOST http://localhost:9130/_/proxy/tenants/diku/modules -d '{"id": "mod-licenses-1.1.0"}'
 popd
--- scripts/run_external_reg.sh
+++ scripts/run_external_reg.sh
@@ -2,5 +2,5 @@
 
 echo Start mod-licenses in external-register mode
 
-java -jar build/libs/mod-licenses-1.0.jar -Xmx1G --server.port=8081 --grails.server.host=10.0.2.2 --dataSource.username=folio_admin --dataSource.password=folio_admin --dataSource.url=jdbc:postgresql://localhost:54321/okapi_modules
+java -jar build/libs/mod-licenses-1.1.jar -Xmx1G --server.port=8080 --grails.server.host=10.0.2.2 --dataSource.username=folio_admin --dataSource.password=folio_admin --dataSource.url=jdbc:postgresql://localhost:54321/okapi_modules
 
