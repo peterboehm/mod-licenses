e63836379bdac961547fb53cb4f18c0831b49ec1
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2019-02-11 16:32:19 +0000

    Resinstate the full file as liquibase is sensitive to filename.

--- service/grails-app/migrations/update-mod-license-1-1-0.groovy
+++ service/grails-app/migrations/update-mod-license-1-1-0.groovy
@@ -1,17 +1,4 @@
 databaseChangeLog = {
-
-  // Put changes in here for 1.1.0
-  changeSet(author: "sosguthorpe", id: "1549536544449-1") {
-    addColumn (tableName: "license" ) {
-      column(name: "lic_end_date", type: "timestamp")
-    }
-  }
-  changeSet(author: "sosguthorpe", id: "1549536544449-2") {
-    addColumn (tableName: "license" ) {
-      column(name: "lic_start_date", type: "timestamp")
-    }
-  }
-
   changeSet(author: "Ian Ibbotson", id: "2019-02-11-0001") {
     addColumn(tableName: "license") {
       column(name: "lic_end_date_semantics_fk", type: "VARCHAR(36)")
