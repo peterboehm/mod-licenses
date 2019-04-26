98eecf4d43295190387b9949b71914255d9db7a7
Ian Ibbotson <ianibbo@gmail.com>
2019-04-26 14:12:07 +0100

    added example of order of deletes to nuke custprops config

--- scripts/handy.sql
+++ scripts/handy.sql
@@ -6,3 +6,16 @@ select * from license;
 
 
 
+
+# nuke custprops
+
+delete from custom_property_blob;
+delete from custom_property_boolean;
+delete from custom_property_container;
+delete from custom_property_decimal;
+delete from custom_property_integer;
+delete from custom_property_refdata;
+delete from custom_property_refdata_definition;
+delete from custom_property_text;
+delete from custom_property;
+delete from custom_property_definition;
