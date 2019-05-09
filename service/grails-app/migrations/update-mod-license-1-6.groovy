890a58ee0ff463104d3cf6c75cb06b34d454bf48
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2019-05-09 13:13:54 +0100

    Migration was incorrect.

--- service/grails-app/migrations/update-mod-license-1-6.groovy
+++ service/grails-app/migrations/update-mod-license-1-6.groovy
@@ -1,7 +1,7 @@
 databaseChangeLog = {
   changeSet(author: "Steve Osguthorpe", id: "2019-05-08-ERM-6") {
-    createTable(tableName: "internal_contact") {
-        column(name: "custom_property", type: "TEXT")
+    addColumn(tableName: "custom_property") {
+      column(name: "note", type: "TEXT")
     }
   }
 }
