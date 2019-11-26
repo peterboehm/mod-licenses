031dcff789a58d8564087900746c46cfb08e3b47
sosguthorpe <steve.osguthorpe@k-int.com>
2019-11-26 17:45:54 +0000

    Feature/file blob streaming (#111)
    Provides [ERM-346]
    
    * Change to TK file handling.
    
    * Hot fix broken migrations for v1.11
    
    * Add the migration changes.

--- service/grails-app/controllers/org/olf/licenses/UrlMappings.groovy
+++ service/grails-app/controllers/org/olf/licenses/UrlMappings.groovy
@@ -1,7 +1,5 @@
 package org.olf.licenses
 
-import org.olf.general.FileUpload
-
 class UrlMappings {
 
   static mappings = {
@@ -47,13 +45,13 @@ class UrlMappings {
         "/find/$id"(controller:'org', action:'find')
       }
     }
-
     
-    get "/licenses/files/$id/raw"(controller: "fileUpload", action: "getFileUploadRaw")
-    get "/licenses/files/$id"(controller: "fileUpload", action: "getFileUpload")
-    get '/licenses/files'(controller: "fileUpload", action: "getFileUploadList")
-    post '/licenses/files'(controller: "fileUpload", action: "postFileUploadRaw")
-    delete "/licenses/files/$id"(controller: "fileUpload", action: "deleteFileUpload")
+    "/licenses/files" ( resources:'fileUpload', excludes: ['update', 'patch', 'save', 'edit', 'create']) {
+      collection {
+        '/' (controller: "fileUpload", action: "uploadFile", method: 'POST')
+      }
+      "/raw" ( controller: "fileUpload", action: "downloadFile", method: 'GET' )
+    }
 
     "500"(view: '/error')
     "404"(view: '/notFound')
