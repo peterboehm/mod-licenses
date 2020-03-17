8a220c474aec389309e2540219c19d808c3fde98
EthanFreestone <ethan.freestone@k-int.co.uk>
2020-03-17 15:18:36 +0000

    tweaked the migration

--- service/grails-app/migrations/update-mod-license-2-2.groovy
+++ service/grails-app/migrations/update-mod-license-2-2.groovy
@@ -2,8 +2,8 @@ databaseChangeLog = {
   changeSet(author: "efreestone (manual)", id: "202003171414-1") {
       modifyDataType( 
         tableName: "custom_property_definition", 
-        columnName: "pd_description", 
-        newDataType: "CLOB",
+        columnName: "pd_description", type: "text",
+        newDataType: "text",
         confirm: "Successfully updated the pd_description column."
       )
   }
